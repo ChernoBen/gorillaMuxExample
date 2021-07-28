f8a1ab42b276a6b56e073b4eb99a893d32574839
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-27 21:46:43 -0400

    feat: routes & mux instance

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crud/servidor"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// CREATE, READ, UPDATE, DELETE
+	// utilizando pacote mux
+
+	// Primeiro passo criar um router
+	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscaUsuario).Methods("GET")
+	// utilizar pacote hhtp para subir o servidor junto com o router
+
+	fmt.Println("Escutando porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+
+}
